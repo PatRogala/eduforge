cc25c49710a39e5340ab84aae15a79c20e8223db
Patryk Rogala <patryk@astarium.pl>
2025-03-01 13:04:38 +0100

    ✨ (sidebar_component): add previews for guest and logged-in user states in SidebarComponent ✨ (sidebar_component.pl.yml): add Polish translation for sign_out ♻️ (sidebar_component): refactor bottom_links method to handle user state more clearly
    New previews for the SidebarComponent allow for better testing of different user states, enhancing the development experience. The Polish translation file now includes a sign_out entry, improving localization support. The bottom_links method is refactored to clearly differentiate between logged-in and non-logged-in users, making the code more readable and maintainable.

--- app/components/previews/sidebar_component_preview.rb
+++ app/components/previews/sidebar_component_preview.rb
@@ -1,6 +1,10 @@
 # Lookbook preview for Sidebar::SidebarComponent
 class SidebarComponentPreview < ViewComponent::Preview
-  def default
+  def guest_user
     render(Sidebar::SidebarComponent.new(current_user: nil))
+  end
+
+  def logged_in_user
+    render(Sidebar::SidebarComponent.new(current_user: User.new))
   end
 end
