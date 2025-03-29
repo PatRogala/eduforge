07725673ea7885b1c5ecbc4b1fbfdb266e902f34
Patryk Rogala <patryk@astarium.pl>
2025-03-29 18:15:34 +0100

    refactor(db): update datetime fields to use timestamptz
    Standardize datetime fields across the database to use `timestamptz` for consistency and better timezone handling. Also, added logging messages to the seed file for clarity during the seeding process.

--- config/initializers/postgres.rb
+++ config/initializers/postgres.rb
@@ -1,3 +1,3 @@
 require "active_record/connection_adapters/postgresql_adapter.rb"
 
-ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::NATIVE_DATABASE_TYPES[:datetime] = { name: "timestamptz" }
+ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.datetime_type = :timestamptz
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
+puts "Creating default roles..."
 Role.create!(id: 1, name: "Admin")
 Role.create!(id: 2, name: "Instructor")
+puts "Roles created!"
