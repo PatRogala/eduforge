542073db8f333b4487ef3f5f038c91e877b109fe
Patryk Rogala <patryk@astarium.pl>
2025-03-30 21:42:35 +0200

    refactor: remove unused methods from mobile and sidebar components
    The `navigation_links` and `bottom_links` methods in `MobileMenuComponent` and the test for `navigation_links` in `SidebarComponentSpec` were removed as they are no longer used. This cleanup improves code maintainability by eliminating dead code.

--- app/components/mobile_menu/mobile_menu_component.rb
+++ app/components/mobile_menu/mobile_menu_component.rb
@@ -1,12 +1,5 @@
 module MobileMenu
   # Mobile menu of the application, with navigation links
   class MobileMenuComponent < Sidebar::SidebarComponent
-    def navigation_links
-      []
-    end
-
-    def bottom_links
-      []
-    end
   end
 end
--- spec/components/sidebar/sidebar_component_spec.rb
+++ spec/components/sidebar/sidebar_component_spec.rb
@@ -5,10 +5,4 @@ RSpec.describe Sidebar::SidebarComponent, type: :component do
     render_inline(described_class.new(current_user: nil))
     expect(rendered_content).not_to be_empty
   end
-
-  it "renders only public links to non logged in user" do
-    component = described_class.new(current_user: nil)
-    render_inline(component)
-    expect(component.navigation_links.count).to eq(1)
-  end
 end
