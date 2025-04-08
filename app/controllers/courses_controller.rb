1eda9d97c975623dd08e161af28e667199d426a1
Patryk Rogala <patryk@astarium.pl>
2025-04-08 17:16:36 +0200

    refactor(courses_controller): remove unused show action
    The show action was not being used in the application, so it has been removed to clean up the code and improve maintainability

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,8 +3,4 @@ class CoursesController < ApplicationController
   def index
     @courses = ProgrammingCourse.with_rich_text_description.all
   end
-
-  def show
-    @course = ProgrammingCourse.find(params[:id])
-  end
 end
