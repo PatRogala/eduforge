9bfae97e242a9425e09ccdb9653706c5bfc0f857
Patryk Rogala <patryk@astarium.pl>
2025-04-14 09:17:12 +0200

    Unused console log

--- app/javascript/controllers/inline_code_trix_controller.js
+++ app/javascript/controllers/inline_code_trix_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="inline-code-trix"
 export default class extends Controller {
   connect() {
-    console.log("inline code trix controller connected")
     Trix.config.textAttributes.inlineCode = { 
       tagName: "code", 
       inheritable: true 
