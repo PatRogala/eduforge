f479d94987f638ecf42bba4fb37749abb425e2fd
Patryk Rogala <rogala.patryk@pm.me>
2025-04-29 22:41:20 +0200

    Refactor header component preview to use a stubbed user instance
    - Updated the logged_in_user method in HeaderComponentPreview to utilize a stubbed user created with FactoryBot, enhancing the realism of the preview for logged-in users.

--- app/previews/header_component_preview.rb
+++ app/previews/header_component_preview.rb
@@ -1,7 +1,8 @@
 # Lookbook preview for Layout::HeaderComponent
 class HeaderComponentPreview < ViewComponent::Preview
   def logged_in_user
-    render(Layout::HeaderComponent.new(current_user: User.first))
+    user = FactoryBot.build_stubbed(:user)
+    render(Layout::HeaderComponent.new(current_user: user))
   end
 
   def logged_out_user
