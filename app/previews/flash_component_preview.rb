3dbd3eede98d35a225ecffdd29e2a0879f8fea19
Patryk Rogala <rogala.patryk@pm.me>
2025-04-30 10:32:47 +0200

    Enhance flash message component with updated styles and Polish translations
    - Refactored the HTML structure of the flash message component to improve styling and responsiveness.
    - Added new SVG icons for success and alert messages.
    - Introduced Polish translations for success and alert messages in a new YAML file.
    - Updated component previews to reflect the new messages and translations.

--- app/previews/flash_component_preview.rb
+++ app/previews/flash_component_preview.rb
@@ -1,10 +1,10 @@
 # Lookbook preview for Flash::FlashMessageComponent
 class FlashComponentPreview < ViewComponent::Preview
   def notice_message
-    render(Flash::FlashMessageComponent.new(notice: "This is notice", alert: nil))
+    render(Flash::FlashMessageComponent.new(notice: "Twój profil został zaktualizowany", alert: nil))
   end
 
   def alert_message
-    render(Flash::FlashMessageComponent.new(notice: nil, alert: "This is alert"))
+    render(Flash::FlashMessageComponent.new(notice: nil, alert: "Nieprawidłowe dane"))
   end
 end
