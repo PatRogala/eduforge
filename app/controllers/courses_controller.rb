0f32877064c4470ea9d953aea1b1f9a94ddb5bde
Patryk Rogala <rogala.patryk@pm.me>
2025-04-30 14:22:16 +0200

    Refactor course-related functionality and add my courses view
    - Renamed the `my_courses` action to `my` in `CoursesController` for consistency.
    - Updated the sidebar component to reflect the new path for the my courses link.
    - Created a new view for displaying the user's enrolled courses.
    - Adjusted routes and Polish translations to align with the new naming convention.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,12 +1,12 @@
 # Generic routes for courses to show them to any user
 class CoursesController < ApplicationController
-  before_action :authenticate_user!, only: %i[my_courses]
+  before_action :authenticate_user!, only: %i[my]
 
   def index
     @courses = ProgrammingCourse.with_rich_text_description.all
   end
 
-  def my_courses
+  def my
     @courses = current_user.enrolled_programming_courses
                            .with_rich_text_description
                            .includes(:instructor, :cover_image_attachment)
